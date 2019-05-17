fd89326ffbf583eb809f3979e721918afda5e915
rubhiauliatirta <rubhiauliatirta@gmail.com>
2019-05-17 11:25:01 +0700

    add base server

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const errorHandler = require('./middlewares/errorHandler')
+const app = express();
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true})
+.then(function(success){
+    console.log('succesfully connect to database')
+})
+.catch(function(err){
+    console.log(err)
+})
+
+app.use('/', require('./routes'));
+app.use(errorHandler);
+
+app.listen(PORT, function(){console.log('listen to port ' + PORT)})
